f557348df68626d7979ffe36093d15d21cc38943
ifacker <2677863724@qq.com>
2022-12-14 17:05:09 +0800

    update v1.2

--- main.go
+++ main.go
@@ -14,5 +14,11 @@ func init() {
 }
 
 func main() {
+	//inputReader := bufio.NewReader(os.Stdin)
+	//fmt.Println("Please enter some input: ")
+	//input, err := inputReader.ReadString('\n')
+	//if err == nil {
+	//	fmt.Printf("The input was: %s\n", input)
+	//}
 	control.Init()
 }
